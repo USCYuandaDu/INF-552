d81a11580c26cfd12c7a9b8d9e37c6cf554f93d9
yuandadu <duyuanda@yeah.net>
2018-03-05 21:25:26 -0800

    linearRegressino done

--- HW4_LinearClassification/linearRegression.py
+++ HW4_LinearClassification/linearRegression.py
@@ -4,4 +4,13 @@ import numpy as np
 
 filename = sys.argv[1]
 data = np.loadtxt(filename, dtype = "float", delimiter = ",")
-print data
+X = np.mat(data[:,0:2])
+Y = data[:,2]
+Y = np.mat(Y[:, np.newaxis])
+xTx = X.T * X
+if (np.linalg.det(xTx) == 0):
+	print "This matrix is singular, cannot do inverse"
+w = xTx.I * (X.T * Y)
+w = w.T
+print w
+
